8e675364a6a7e9ad854ef4141cb908231ffe7e53
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-08-16 14:09:47 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center text-2xl">
-      Sorry! Website in development
+      Sorry! Website in development.
     </div>
   )
 }
